2a8754049668817b394dd5619805938b420fdaf6
Gokul Krishna <gk023785@gmail.com>
2026-06-26 23:21:35 +0530

    Update next.config.ts

--- localfix/next.config.ts
+++ localfix/next.config.ts
@@ -3,6 +3,5 @@ import type { NextConfig } from "next";
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  output: "standalone",
 };
 export default nextConfig;
