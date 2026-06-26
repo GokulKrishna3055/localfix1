70e663daae8329ca3407eb53205b0c07e9c39ce9
Gokul Krishna <gk023785@gmail.com>
2026-06-26 21:21:44 +0530

    Remove experimental missingSuspenseWithCSRBailout option

--- localfix/next.config.ts
+++ localfix/next.config.ts
@@ -4,6 +4,5 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   output: "standalone",
-  experimental: { missingSuspenseWithCSRBailout: false },
 };
 export default nextConfig;
